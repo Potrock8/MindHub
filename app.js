f667763a4284df829a7b8120edad2cb84657986c
Potrock8 <patrick_elijah_tan@dlsu.edu.ph>
2022-07-09 02:28:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ database.connect(url);
 
 app.use(session({
     secret: key,
-    store: MongoStore.create({client: database)},
+    store: MongoStore.create({client: database}),
     resave: false,
     saveUninitialized: true,
     cookie: {secure: false, maxAge: 1000 * 60 * 60 * 24 * 7},
